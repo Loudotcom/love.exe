d99853889dbb4905ffa7eb7b4d7a1dd8eba2f3a7
Michael Cornall <mik.reds.forever@gmail.com>
2025-02-14 22:11:23 +0000

    setup dj-database-url, psycopg2, linking to external postgres db

--- matching_project/matching_project/settings.py
+++ matching_project/matching_project/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 import environ
+import dj_database_url
+
 
 env = environ.Env()
 
@@ -81,10 +83,7 @@ WSGI_APPLICATION = 'matching_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(env("DATABASE_URL"))
 }
 
 
