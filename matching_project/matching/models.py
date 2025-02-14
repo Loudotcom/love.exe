a3eead18384e708880f2958ee95fc3e10d0c686a
TeodoraAlina <115451011+TeodoraAlina@users.noreply.github.com>
2025-02-14 12:26:11 +0100

    Add CustomUser and Hobby models for user registration

--- matching_project/matching/models.py
+++ matching_project/matching/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    age = models.IntegerField(null=True, blank=True)
+    location = models.CharField(max_length=255)
+    profile_picture = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
+    hobbies = models.ManyToManyField('Hobby', related_name='users')
+    
+    def __str__(self):
+        return self.username
+
+
+class Hobby(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
