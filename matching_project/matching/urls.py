161caf53aa150067a7b322732da6032431380ab9
TeodoraAlina <115451011+TeodoraAlina@users.noreply.github.com>
2025-02-16 02:11:03 +0100

    Update answer_questions template to redirect the user to the profile detail if answer is true

--- matching_project/matching/urls.py
+++ matching_project/matching/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
     path('update-profile/', views.update_profile, name='update-profile'),
     path('profile/', views.profile, name='profile'),
-    # path('profile/<int:user_id>/', views.profile_detail, name='profile_detail'),
     path('match/', views.matching_view, name='match'),
     path('get_dealbreaker_questions/<int:profile_id>/', views.get_dealbreaker_questions, name='get_dealbreaker_questions'),
     path('answer-dealbreaker/<int:profile_id>/', views.answer_dealbreaker_questions, name='answer_dealbreaker_questions'),
